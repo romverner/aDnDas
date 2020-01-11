6d40a1364ea0199ff3019d9661dde1be8b956bce
Connor Archard <carchard0@gmail.com>
2020-01-11 16:07:24 -0500

    spaces instead of tabs!

--- env.py
+++ env.py
@@ -5,15 +5,15 @@ env_const = 42
 
 
 class EnvObject():
-	def __init__(self, x_pos, y_pos):
-		self.x_pos = x_pos
-		self.y_pos = y_pos
+    def __init__(self, x_pos, y_pos):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
 
-	def do_work(self):
-		self.x_pos = self.x_pos + 2
-		
+    def do_work(self):
+        self.x_pos = self.x_pos + 2
+        
 
 if __name__ == "__main__":
-	logging.info("in main of env")
-	eo = EnvObject(4, 8)
-	eo.do_work()
+    logging.info("in main of env")
+    eo = EnvObject(4, 8)
+    eo.do_work()
--- game.py
+++ game.py
@@ -7,19 +7,19 @@ import env
 import constants as _c
 
 if __name__ == "__main__":
-	pygame.init()
-	clock = pygame.time.Clock()
-	logging.debug("env constants: {}".format(env.env_const))
-	status = _c.RUNNING_STATUS
+    pygame.init()
+    clock = pygame.time.Clock()
+    logging.debug("env constants: {}".format(env.env_const))
+    status = _c.RUNNING_STATUS
 
-	while status not in [_c.QUIT_STATUS, _c.CRASHED_STATUS]:
-		# do things here that reflect game updates
+    while status not in [_c.QUIT_STATUS, _c.CRASHED_STATUS]:
+        # do things here that reflect game updates
 
-		# event handlers
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				status = _c.QUIT_STATUS
-				logging.debug("")
+        # event handlers
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                status = _c.QUIT_STATUS
+                logging.debug("")
 
-		# wait for the next frame
-		clock.tick(_c.FPS)
+        # wait for the next frame
+        clock.tick(_c.FPS)
